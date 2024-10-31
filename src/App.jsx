f597ee75c428d681f22fb94490bcbf897c67973c
Muhammad Ahsan <itsmahsank@gmail.com>
2024-10-31 17:47:19 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { HeroSection } from "./components/HeroSection";
+import { Navbar } from "./components/Navbar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection /> 
+  
+    </>
+  );
+};
+
+export default App;
